576a503b2dae5099f78dbbd940eabe324999ef73
川端洋平 <bata44883@gmail.com>
2015-05-23 02:01:36 +0900

    staging設定追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:e-kikai/e-kikai_02.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/var/www/e-kikai_02'
+set :deploy_to, '/var/www/e-kikai'
 
 # Default value for :scm is :git
 set :scm, :git

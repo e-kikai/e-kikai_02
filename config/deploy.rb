90157e3af567de724c1b511d220c08b79896f87d
川端洋平 <bata44883@gmail.com>
2015-05-30 03:08:42 +0900

    config修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,9 +24,11 @@ set :scm, :git
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }

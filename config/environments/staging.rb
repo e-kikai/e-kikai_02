b91ed2c03d944ac94126d9100cb8323d65fa61f4
川端洋平 <bata44883@gmail.com>
2015-05-31 01:19:20 +0900

    cache_template_loading = false

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -25,9 +25,10 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-
+  config.action_view.cache_template_loading = false
+  
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 

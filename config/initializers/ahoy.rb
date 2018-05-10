a1692425b728e3871c89abfc8ae61f7f3d145f50
川端洋平 <bata44883@gmail.com>
2018-05-10 22:52:03 +0900

    Ahoy5

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,7 +1,7 @@
-# class Ahoy::Store < Ahoy::Stores::ActiveRecordTokenStore
-#   # customize here
-#   def exclude?
-#     bot? || request.ip == "192.168.1.1"
-#   end
-#
-# end
+class Ahoy::Store < Ahoy::Stores::ActiveRecordTokenStore
+  # customize here
+  def exclude?
+    bot? || request.ip == "192.168.1.1"
+  end
+
+end

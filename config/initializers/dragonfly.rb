7353e083cd610ceb425313c5bd26e7ca64d2213a
川端洋平 <bata44883@gmail.com>
2018-11-13 14:31:21 +0900

    マシンライフのリニューアル対応

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,26 +1,26 @@
-require 'dragonfly'
+# require 'dragonfly'
 
 # Configure
-Dragonfly.app.configure do
-  plugin :imagemagick
-
-  secret "773cfcb382cd107cc8d1559fac0e345eea1f82299214525af1b541b52e32abbe"
-
-  url_format "/media/:job/:name"
-
-  datastore :file,
-    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
-    server_root: Rails.root.join('public')
-end
-
-# Logger
-Dragonfly.logger = Rails.logger
-
-# Mount as middleware
-Rails.application.middleware.use Dragonfly::Middleware
-
-# Add model functionality
-if defined?(ActiveRecord::Base)
-  ActiveRecord::Base.extend Dragonfly::Model
-  ActiveRecord::Base.extend Dragonfly::Model::Validations
-end
+# Dragonfly.app.configure do
+#   plugin :imagemagick
+#
+#   secret "773cfcb382cd107cc8d1559fac0e345eea1f82299214525af1b541b52e32abbe"
+#
+#   url_format "/media/:job/:name"
+#
+#   datastore :file,
+#     root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+#     server_root: Rails.root.join('public')
+# end
+#
+# # Logger
+# Dragonfly.logger = Rails.logger
+#
+# # Mount as middleware
+# Rails.application.middleware.use Dragonfly::Middleware
+#
+# # Add model functionality
+# if defined?(ActiveRecord::Base)
+#   ActiveRecord::Base.extend Dragonfly::Model
+#   ActiveRecord::Base.extend Dragonfly::Model::Validations
+# end
